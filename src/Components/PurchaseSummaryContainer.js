aa1cef9c0f0a41635cd611728ac178ed60ab3e49
Megha Gupta <gupta34@email.sc.edu>
2019-02-27 22:18:58 -0800

    Move promo code to redux action

--- src/Components/PurchaseSummaryContainer.js
+++ src/Components/PurchaseSummaryContainer.js
@@ -1,48 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchItemData, getItem } from "../redux/ducks/item";
+import {applyPromoCode, fetchItemData, getItem} from '../redux/ducks/item';
 import PurchaseSummary from "./PucharseSummary";
 
 class PurchaseSummaryContainer extends Component {
-  state = {
-    item: null
-  };
-
-  promotionCodes = {
-    DISCOUNT: 0.1
-  };
-
   componentDidMount() {
     this.props.fetchItemData();
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (!prevProps.item && this.props.item) {
-      this.setState({item: this.props.item})
-    }
-  }
-
-  applyPromotionCode = code => {
-    return this.setState(({item}) => ({
-      item: {
-        ...item,
-        pricing: {
-          ...item.pricing,
-          total: this.promotionCodes[code]
-              ? (1 - this.promotionCodes[code]) * this.props.item.pricing.total
-              : this.props.item.pricing.total
-        }
-      }
-    }));
-  };
-
   render() {
-    const { item } = this.state;
+    const { applyPromoCode, item } = this.props;
 
     return item ? (
       <PurchaseSummary
         item={item}
-        onApplyPromotionCode={this.applyPromotionCode}
+        onApplyPromotionCode={applyPromoCode}
       />
     ) : null;
   }
@@ -55,6 +27,7 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   {
-    fetchItemData
+    fetchItemData,
+    applyPromoCode,
   }
 )(PurchaseSummaryContainer);
--- src/redux/ducks/item.js
+++ src/redux/ducks/item.js
@@ -1,14 +1,28 @@
 import mockItem, { getPricingData } from "../../data/item";
 
+const promotionCodes = {
+  DISCOUNT: 0.1
+};
+
 // Action types
 const FETCH_ITEM_DATA = "FETCH_ITEM_DATA";
+const APPLY_PROMO_CODE = "APPLY_PROMO_CODE";
 
 // Action creators
 export const fetchItemData = () => dispatch => {
-  return getPricingData().then(result => dispatch({
-    type: FETCH_ITEM_DATA,
-    payload: result
-  }));
+  return getPricingData().then(result =>
+    dispatch({
+      type: FETCH_ITEM_DATA,
+      payload: result
+    })
+  );
+};
+
+export const applyPromoCode = code => dispatch => {
+  dispatch({
+    type: APPLY_PROMO_CODE,
+    payload: code
+  });
 };
 
 // Selectors
@@ -18,6 +32,22 @@ const item = (state = null, action) => {
   switch (action.type) {
     case FETCH_ITEM_DATA:
       return action.payload;
+    case APPLY_PROMO_CODE:
+      const item = state;
+      const code = action.payload;
+      const discount = promotionCodes[code];
+
+      return item
+        ? {
+            ...item,
+            pricing: {
+              ...item.pricing,
+              total: discount
+                ? (1 - discount) * item.pricing.total
+                : item.pricing.total
+            }
+          }
+        : null;
     default:
       return state;
   }
